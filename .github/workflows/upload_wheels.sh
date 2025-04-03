728c5b8c5e4a81b1fd83d28d13779fd4f1e79c06
StijnWoestenborghs <stijn.woestenborghs@live.be>
2025-04-03 14:41:26 +0200

    fix upload script [build wheel]

--- .github/workflows/upload_wheels.sh
+++ .github/workflows/upload_wheels.sh
@@ -24,21 +24,23 @@ upload_wheels() {
     if [[ ${WHL_UPLOAD} == true ]]; then
         if [ -z ${TOKEN} ]; then
             echo no token set, not uploading
-        # sdists are located under dist folder
-        if compgen -G "./dist/*.gz"; then
-            echo "Found sdist"
-            echo "Uploading sdist to ${UPLOAD_REGISTRY}"
-            # anaconda -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./dist/*.gz
-        elif compgen -G "./wheelhouse/*.whl"; then
-            echo "Found wheel"
-            # Force a replacement if the remote file already exists -
-            # nightlies will not have the commit ID in the filename, so
-            # are named the same (1.X.Y.dev0-<platform/interpreter-tags>)
-            echo "Uploading wheel to ${UPLOAD_REGISTRY}"
-            #anaconda -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./wheelhouse/*.whl
         else
-            echo "Files do not exist"
-            return 1
+            # sdists are located under dist folder
+            if compgen -G "./dist/*.gz"; then
+                echo "Found sdist"
+                echo "Uploading sdist to ${UPLOAD_REGISTRY}"
+                # anaconda -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./dist/*.gz
+            elif compgen -G "./wheelhouse/*.whl"; then
+                echo "Found wheel"
+                # Force a replacement if the remote file already exists -
+                # nightlies will not have the commit ID in the filename, so
+                # are named the same (1.X.Y.dev0-<platform/interpreter-tags>)
+                echo "Uploading wheel to ${UPLOAD_REGISTRY}"
+                #anaconda -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./wheelhouse/*.whl
+            else
+                echo "Files do not exist"
+                return 1
+            fi
         fi
     fi
 }
